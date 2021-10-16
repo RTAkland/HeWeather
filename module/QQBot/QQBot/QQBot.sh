d86f61a279fae3748ef6f0d4016ef64413508563
RTAkland <markushammered@gmail.com>
2021-10-16 15:57:37 +0800

    Added QQBOT

--- module/QQBot/QQBot/QQBot.sh
+++
@@ -1,3 +0,0 @@
-echo "Running QQBot"
-chmod +x go-cqhttp
-./go-cqhttp
--- module/QQBot/test'.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: markushammered@gmail.com
-# @Development Tool: PyCharm
-# @Create Time: 2021/10/16
-# @File Name: test'.py
-
