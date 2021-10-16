2492f865cd75dd7fcf48c11f5e6df727ef02fd13
RTAkland <markushammered@gmail.com>
2021-10-16 17:28:07 +0800

    Added OS selector(Auto)

--- module/QQBot/download-QQBot-core.py
+++ module/QQBot/download-QQBot-core.py
@@ -6,13 +6,22 @@
 # @File Name: download-QQBot-core.py
 
 import requests
+import os
 
-windows_core = requests.get('https://gitee.com/rtakland/prefile-repo/raw/main/go-cqhttp.exe')
-with open('go-cqhttp.exe', 'wb') as f:
-    f.write(windows_core.content)
+def down(system):
+    if system == 'nt':
+        windows_core = requests.get(
+            'https://codechina.csdn.net/GeminiTay/some-rawpic/-/raw/master/go-cqhttp-w?inline=false')
+        with open('go-cqhttp.exe', 'wb') as f:
+            f.write(windows_core.content)
+    else:
+        linux_core = requests.get(
+            'https://codechina.csdn.net/GeminiTay/some-rawpic/-/raw/master/go-cqhttp-linux?inline=false')
+        with open('go-cqhttp-linux', 'wb') as f:
+            f.write(linux_core.content)
 
-linux_core = requests.get('https://gitee.com/rtakland/prefile-repo/raw/main/go-cqhttp-linux')
-with open('go-cqhttp-linux', 'wb') as f:
-    f.write(linux_core.content)
 
-print('执行完成...')
+if __name__ == '__main__':
+    print('正在下载文件中...')
+    down(os.name)
+    print('执行完成...')
