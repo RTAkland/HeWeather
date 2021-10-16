40acb807fbda6f2c2e4444c85b256e040199faf6
RTAkland <markushammered@gmail.com>
2021-10-16 17:53:25 +0800

    update some codes

--- module/QQBot/download-QQBot-core.py
+++ module/QQBot/download-QQBot-core.py
@@ -22,6 +22,15 @@ def down(system):
 
 
 if __name__ == '__main__':
-    print('正在下载文件中...')
-    down(os.name)
-    print('执行完成...')
+    OS = os.name
+    if OS == 'nt':
+        if os.path.isfile('go-cqhttp.exe'):
+            print('正在下载文件中...')
+            down(OS)
+            print('文件下载完成...')
+    else:
+        if os.path.isfile('go-cqhttp-linux'):
+            print('正在下载文件中...')
+            down(OS)
+            print('文件下载完成...')
+
